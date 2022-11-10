337e0dfd533530de4df1bf6c482b7e5064a1e184
ahmettahasener <tahaxsener@gmail.com>
2022-11-11 01:03:45 +0300

    Game Uploaded
    The game in the Unity Lessons.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Private Variables
+    public float speed;
+    public float turnSpeed;
+    private float horizontalInput;
+    private float verticalInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This is where we get player input.
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        //We move the vehicle forward.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //We turn the vehicle.
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
